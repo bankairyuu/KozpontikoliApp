7ac493f3e21b3bfebdd445d7a9025ab156322290
Krisz-Lenovo <bankairyuu@gmail.com>
2015-02-26 15:03:05 +0100

    Proxy beállítások megírva, csak a koliweblap megnyitása és a kondi, meg a proxyhoz kötött mosógépes cucc megírása van hátra, aztán csókolom :D

--- KozpontiApp/MainWindow.xaml.cs
+++ KozpontiApp/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Diagnostics;
+
 
 namespace KozpontiApp
 {
@@ -20,6 +24,12 @@ namespace KozpontiApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+        static bool settingsReturn, refreshReturn;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,15 +42,41 @@ namespace KozpontiApp
 
         private void KoliProxy_Click(object sender, RoutedEventArgs e)
         {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            registry.SetValue("ProxyEnable", 1);
+            registry.SetValue("ProxyServer", "proxy.vekoll.uni-pannon.hu:3128");
 
+            // These lines implement the Interface in the beginning of program 
+            // They cause the OS to refresh the settings, causing IP to realy update
+            settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+            MessageBox.Show("Koli proxy beállítva!");
         }
 
         private void ProxyKi_Click(object sender, RoutedEventArgs e)
         {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            registry.SetValue("ProxyEnable", 0);
 
+            // These lines implement the Interface in the beginning of program 
+            // They cause the OS to refresh the settings, causing IP to realy update
+            settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+            MessageBox.Show("Proxy kikapcsolva!");
         }
 
         private void MásProxy_Click(object sender, RoutedEventArgs e)
+        {
+            new MásikProxy().Show();
+        }
+
+        private void Kondi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void Weblap_Click(object sender, RoutedEventArgs e)
         {
 
         }
