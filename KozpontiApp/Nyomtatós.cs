9f421edfe313affa62584b0efe6b4f9c1ae3c61b
Krisz-Lenovo <bankairyuu@gmail.com>
2015-02-27 13:46:42 +0100

    nyomtatós is meghívásra kész (de nincs bekötve), csak a főoldal kódját kell még túrni, aztán gyakorlatilag kész is van

--- KozpontiApp/Nyomtatós.cs
+++ KozpontiApp/Nyomtatós.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace KozpontiApp
 {
-    class Nyomtatós
+    class Nyomtatós : Főoldalolvasó
     {
+        Button b;
+
+        public Nyomtatós(Button b)
+        {
+            this.b = b;
+            színezés(b, lenyeg(3));
+        }
     }
 }
